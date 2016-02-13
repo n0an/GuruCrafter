dcc2b2113760362bddb93e4cb7fa5a2ca707dea9
n0an <nantog@gmail.com>
2016-02-13 10:54:52 +0300

    Added ANMessagesViewController

---
+++ 46_APIWithTokenHomework/ANMessagesViewController.h
@@ -0,0 +1,17 @@
+//
+//  ANMessagesViewController.h
+//  46_APIWithTokenHomework
+//
+//  Created by Anton Novoselov on 13/02/16.
+//  Copyright © 2016 Anton Novoselov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ANMessagesViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+
+
+@end
---
+++ 46_APIWithTokenHomework/ANMessagesViewController.m
@@ -0,0 +1,32 @@
+//
+//  ANMessagesViewController.m
+//  46_APIWithTokenHomework
+//
+//  Created by Anton Novoselov on 13/02/16.
+//  Copyright © 2016 Anton Novoselov. All rights reserved.
+//
+
+#import "ANMessagesViewController.h"
+
+@interface ANMessagesViewController ()
+
+@end
+
+
+
+
+@implementation ANMessagesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
